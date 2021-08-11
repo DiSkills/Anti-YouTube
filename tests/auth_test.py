894793dfc4d52a66bf004dd9a60854b27c534709
counter0021 <ara123rus123ara@gmail.com>
2021-08-11 14:29:45 +0300

    Test register

---
+++ tests/auth_test.py
@@ -0,0 +1,139 @@
+import asyncio
+from unittest import TestCase
+
+from fastapi import HTTPException
+from fastapi.testclient import TestClient
+
+from app.app import app
+from app.auth.api import register
+from app.auth.crud import user_crud, verification_crud
+from app.auth.schemas import RegisterUser
+from app.config import API_V1_URL
+from app.db import Base, engine, AsyncSession
+
+
+async def create_all():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def drop_all():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+class AuthTestCase(TestCase):
+
+    def loop(self, function):
+        loop = asyncio.get_event_loop()
+        try:
+            return loop.run_until_complete(function)
+        finally:
+            pass
+
+    def setUp(self) -> None:
+        self.session = AsyncSession(engine)
+        self.client = TestClient(app)
+        self.data = {
+            'password': 'test1234',
+            'confirm_password': 'test1234',
+            'username': 'test',
+            'email': 'test@example.com',
+            'about': 'string',
+            'send_message': True
+        }
+        self.url = API_V1_URL + '/auth'
+        self.loop(create_all())
+
+    def tearDown(self) -> None:
+        self.loop(self.session.close())
+        self.loop(drop_all())
+
+    def test_register_request(self):
+
+        self.assertEqual(self.loop(user_crud.exists(self.session, id=1)), False)
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 0)
+        self.assertEqual(len(self.loop(verification_crud.all(self.session))), 0)
+        self.assertEqual(self.loop(verification_crud.exists(self.session, id=1)), False)
+
+        response = self.client.post(self.url + '/register', json=self.data)
+
+        self.assertEqual(self.loop(user_crud.exists(self.session, id=1)), True)
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 1)
+        self.assertEqual(len(self.loop(verification_crud.all(self.session))), 1)
+        self.assertEqual(self.loop(verification_crud.exists(self.session, id=1)), True)
+        self.assertEqual(response.json(), {'msg': 'Send email for activate account'})
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.post(self.url + '/register', json=self.data)
+        self.assertEqual(response.json(), {'detail': 'Username exist'})
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 1)
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(self.url + '/register', json={**self.data, 'username': 'test2'})
+        self.assertEqual(response.json(), {'detail': 'Email exist'})
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 1)
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.post(self.url + '/register', json={**self.data, 'username': 'test2', 'email': 'test'})
+        self.assertEqual(response.json()['detail'][0]['msg'], 'value is not a valid email address')
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 1)
+        self.assertEqual(response.status_code, 422)
+
+        response = self.client.post(
+            self.url + '/register', json={
+                **self.data, 'username': 'test2', 'email': 'test@test.com', 'password': 'test'
+            }
+        )
+        self.assertEqual(response.json()['detail'][0]['msg'], 'Min length password is 8 chars')
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 1)
+        self.assertEqual(response.status_code, 422)
+
+        response = self.client.post(
+            self.url + '/register', json={
+                **self.data, 'username': 'test2', 'email': 'test@test.com', 'confirm_password': 'test'
+            }
+        )
+        self.assertEqual(response.json()['detail'][0]['msg'], 'Passwords do not match')
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 1)
+        self.assertEqual(response.status_code, 422)
+        
+    def test_register(self):
+
+        self.assertEqual(self.loop(user_crud.exists(self.session, id=1)), False)
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 0)
+        self.assertEqual(len(self.loop(verification_crud.all(self.session))), 0)
+        self.assertEqual(self.loop(verification_crud.exists(self.session, id=1)), False)
+
+        response = self.loop(register(RegisterUser(**self.data)))
+
+        self.assertEqual(self.loop(user_crud.exists(self.session, id=1)), True)
+        self.assertEqual(len(self.loop(user_crud.all(self.session))), 1)
+        self.assertEqual(len(self.loop(verification_crud.all(self.session))), 1)
+        self.assertEqual(self.loop(verification_crud.exists(self.session, id=1)), True)
+        self.assertEqual(response, {'msg': 'Send email for activate account'})
+
+        with self.assertRaises(HTTPException) as error:
+            self.loop(register(RegisterUser(**self.data)))
+
+        with self.assertRaises(HTTPException) as error:
+            self.loop(register(RegisterUser(**{**self.data, 'username': 'test2'})))
+
+        with self.assertRaises(ValueError) as error:
+            self.loop(register(RegisterUser(**{**self.data, 'username': 'test2', 'email': 'test'})))
+
+        with self.assertRaises(ValueError) as error:
+            self.loop(
+                register(
+                    RegisterUser(**{**self.data, 'username': 'test2', 'email': 'test@test.com', 'password': 'test'})
+                )
+            )
+
+        with self.assertRaises(ValueError) as error:
+            self.loop(
+                register(
+                    RegisterUser(
+                        **{**self.data, 'username': 'test2', 'email': 'test@test.com', 'confirm_password': 'test'}
+                    )
+                )
+            )
