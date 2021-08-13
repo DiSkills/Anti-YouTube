6df253da33edc4663a3598fc803ed2b526c47a33
counter0021 <ara123rus123ara@gmail.com>
2021-08-13 17:00:02 +0300

    API docs

--- app/auth/api.py
+++ app/auth/api.py
@@ -10,63 +10,126 @@ from app.schemas import Message
 auth_router = APIRouter()
 
 
-@auth_router.post('/refresh', status_code=status.HTTP_200_OK, response_model=AccessToken)
+@auth_router.post(
+    '/refresh',
+    status_code=status.HTTP_200_OK,
+    response_model=AccessToken,
+    description='Refresh token',
+    response_description='Access token',
+    name='Refresh token',
+)
 async def refresh(schema: RefreshToken):
     async with async_session() as session:
         async with session.begin():
             return await service.refresh(session, schema)
 
 
-@auth_router.post('/register', status_code=status.HTTP_201_CREATED, response_model=Message)
+@auth_router.post(
+    '/register',
+    status_code=status.HTTP_201_CREATED,
+    response_model=Message,
+    description='Registration new user',
+    response_description='User has been registration',
+    name='Registration',
+)
 async def register(schema: RegisterUser):
     async with async_session() as session:
         async with session.begin():
             return await service.register(session, schema)
 
 
-@auth_router.post('/activate', status_code=status.HTTP_200_OK, response_model=Message)
+@auth_router.post(
+    '/activate',
+    status_code=status.HTTP_200_OK,
+    response_model=Message,
+    description='Activation account',
+    response_description='Account activated',
+    name='Activation account',
+)
 async def activate(schema: VerificationUUID):
     async with async_session() as session:
         async with session.begin():
             return await service.activate(session, schema)
 
 
-@auth_router.post('/login', response_model=Tokens, status_code=status.HTTP_200_OK)
+@auth_router.post(
+    '/login',
+    response_model=Tokens,
+    status_code=status.HTTP_200_OK,
+    description='Login',
+    response_description='Tokens',
+    name='Login',
+)
 async def login(schema: LoginUser):
     async with async_session() as session:
         async with session.begin():
             return await service.login(session, schema)
 
 
-@auth_router.post('/follow', response_model=Message, status_code=status.HTTP_200_OK)
+@auth_router.post(
+    '/follow',
+    response_model=Message,
+    status_code=status.HTTP_200_OK,
+    description='Follow',
+    response_description='Follow',
+    name='Follow',
+)
 async def follow(to_id: int, user: User = Depends(is_active)):
     async with async_session() as session:
         async with session.begin():
             return await service.follow(session, to_id, user)
 
 
-@auth_router.post('/unfollow', response_model=Message, status_code=status.HTTP_200_OK)
+@auth_router.post(
+    '/unfollow',
+    response_model=Message,
+    status_code=status.HTTP_200_OK,
+    description='Unfollow',
+    response_description='Unfollow',
+    name='Unfollow',
+)
 async def unfollow(to_id: int, user: User = Depends(is_active)):
     async with async_session() as session:
         async with session.begin():
             return await service.unfollow(session, to_id, user)
 
 
-@auth_router.get('/request-password-reset', response_model=Message, status_code=status.HTTP_200_OK)
+@auth_router.get(
+    '/request-password-reset',
+    response_model=Message,
+    status_code=status.HTTP_200_OK,
+    description='Request reset password',
+    response_description='Request reset password',
+    name='Request reset password',
+)
 async def create_reset_password(email: str):
     async with async_session() as session:
         async with session.begin():
             return await service.create_reset_password(session, email)
 
 
-@auth_router.post('/password-reset', response_model=Message, status_code=status.HTTP_200_OK)
+@auth_router.post(
+    '/password-reset',
+    response_model=Message,
+    status_code=status.HTTP_200_OK,
+    description='Reset password',
+    response_description='Reset password',
+    name='Reset password',
+)
 async def verify_password_reset(token: str, schema: Password):
     async with async_session() as session:
         async with session.begin():
             return await service.verify_password_reset(session, token, schema)
 
 
-@auth_router.post('/username', response_model=Message, status_code=status.HTTP_200_OK)
+@auth_router.post(
+    '/username',
+    response_model=Message,
+    status_code=status.HTTP_200_OK,
+    description='Get username from email',
+    response_description='Get username from email',
+    name='Get username',
+)
 async def get_username(email: str):
     async with async_session() as session:
         async with session.begin():
