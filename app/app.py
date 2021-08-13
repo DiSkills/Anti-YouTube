825cc14cf1fd100a00c5b6bc43f2d325c52aefb4
counter0021 <ara123rus123ara@gmail.com>
2021-08-13 18:02:25 +0300

    CORS

--- app/app.py
+++ app/app.py
@@ -10,9 +10,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[''],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=[''],
+    allow_methods=['*'],
     allow_headers=['*'],
 )
 
